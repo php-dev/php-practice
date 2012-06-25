5c3fe0f62d0ebb2d9babd50c754a6f3f83a5a4d9
Anit <shrestha.anit@gmail.com>
2012-06-25 09:05:55 +0545

    commit left of friday

--- yql_test.php
+++ yql_test.php
@@ -1,30 +1,56 @@
-<?php
-$headers = array(             
-        "Content-type: text/xml;charset=\"utf-8\"", 
-        "Accept: text/xml", 
-        "Cache-Control: no-cache", 
-        "Pragma: no-cache", 
-        "SOAPAction: \"run\""
-    ); 
-$url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20upcoming.events%20where%20woeid%20in%20(select%20woeid%20from%20geo.places%20where%20text%3D%22North%20Beach%22)%20%7C%20sort(field%3D%22start_date%22)%20%7C%20truncate(count%3D1)&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-$soap_do = curl_init(); 
-curl_setopt($soap_do, CURLOPT_URL,            $url );   
-curl_setopt($soap_do, CURLOPT_CONNECTTIMEOUT, 10); 
-curl_setopt($soap_do, CURLOPT_TIMEOUT,        10); 
-curl_setopt($soap_do, CURLOPT_RETURNTRANSFER, true );
-curl_setopt($soap_do, CURLOPT_SSL_VERIFYPEER, false);  
-curl_setopt($soap_do, CURLOPT_SSL_VERIFYHOST, false); 
-
-//curl_setopt($soap_do, CURLOPT_POSTFIELDS,    $post_string); 
-curl_setopt($soap_do, CURLOPT_HTTPHEADER,  $headers);   
-curl_setopt($soap_do, CURLOPT_USERPWD, 'pician_friend@yahoo.com' . ":" . "p!(i@n");
-
-$result = curl_exec($soap_do);
-$err = curl_error($soap_do);
-//var_dump($result);
-
-$dom = new DOMDocument;
-$dom->loadXML($result);
-
-
-?>
+<h2>Using YQL to Access the Upcoming API</h2>  
+    <form name='upcoming_form'>  
+    Location: <input name='location' id='location' type='text' size='20'/><br/>  
+    Event: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input name='event' id='event' type='text' size='20'/><br/>  
+    <p><button id='find_event'>Find Event</button></p>  
+    </form>  
+      
+    <script>  
+      // Attach event handler to button  
+      document.getElementById("find_event").addEventListener("click",find_event,false);  
+      // Get user input and submit form  
+      function find_event(){  
+        document.upcoming_form.event.value = document.getElementById('event').value || "music";  
+        document.upcoming_form.location.value = document.getElementById('location').value || "San Francisco";  
+        document.upcoming_form.submit();  
+      }   
+    </script>  
+    <?php  
+      $BASE_URL = "https://query.yahooapis.com/v1/public/yql";  
+      
+      if(isset($_GET['event']) && isset($_GET['location'])){  
+        $location = $_GET['location'];  
+        $query = $_GET['event'];  
+        $events="";  
+           
+        // Form YQL query and build URI to YQL Web service  
+        $yql_query = "select * from upcoming.events where location='$location' and search_text='$query'";  
+        $yql_query_url = $BASE_URL . "?q=" . urlencode($yql_query) . "&format=json";  
+      
+        // Make call with cURL  
+        $session = curl_init($yql_query_url);  
+        curl_setopt($session, CURLOPT_RETURNTRANSFER,true);  
+        $json = curl_exec($session);  
+        // Convert JSON to PHP object   
+        $phpObj =  json_decode($json);  
+      
+        // Confirm that results were returned before parsing  
+        if(!is_null($phpObj->query->results)){  
+          // Parse results and extract data to display  
+          foreach($phpObj->query->results->event as $event){  
+            $events .= "<div><h2>" . $event->name . "</h2><p>";  
+            $events .= html_entity_decode(wordwrap($event->description, 80, "<br/>"));  
+            $events .="</p><br/>$event->venue_name<br/>$event->venue_address<br/>";  
+            $events .="$event->venue_city, $event->venue_state_name";  
+            $events .="<p><a href=$event->ticket_url>Buy Tickets</a></p></div>";  
+          }  
+        }  
+        // No results were returned  
+        if(emptyempty($events)){  
+          $events = "Sorry, no events matching $query in $location";  
+        }  
+        // Display results and unset the global array $_GET  
+        echo $events;  
+        unset($_GET);  
+      }  
+    ?>  
