020da2d33691567936a012392e5ecb82f0106453
anit <shrestha.anit@gmail.com>
2012-11-13 12:37:26 +0545

    none

--- jquery_deferred/ajax.php
+++ jquery_deferred/ajax.php
@@ -2,8 +2,11 @@
 
 if((isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) {
     //var_dump($_POST);     
-    print json_encode(array("firstName"=>'its-true'));
-    
+     print json_encode(array("firstName"=>'its-true'));
+    //$json_obj = json_decode($_POST['json']);    
+    //print $json_obj->firstName;
+}else {
+    echo'I got my eyes on you.';
 }
 
 
