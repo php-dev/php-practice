b05cfce61181850644ac60fd4722d38d971b0cb6
Anit Shrestha <shrestha.anit@gmail.com>
2013-12-16 10:05:49 +0545

    Adapter pattern using Inherientece - Complete

---
+++ SandersW-LearningPHPDesignPatterns/adapter/UsingInheritence/EuroAdapter.php
@@ -0,0 +1,17 @@
+<?php
+//EuroAdapter.php
+//Adapter
+include_once('EuroCalc.php');
+include_once('ITarget.php');
+
+class EuroAdapter extends EuroCalc implements ITarget {
+
+	public function __construct() {
+		$this->requester();
+	}
+
+	function requester() {
+		$this->rate = .8111;
+		return $this->rate;
+	}
+}
---
+++ SandersW-LearningPHPDesignPatterns/adapter/UsingInheritence/ITarget.php
@@ -0,0 +1,6 @@
+<?php
+//ITarget.php
+//Target
+interface ITarget {
+	function requester();
+}
